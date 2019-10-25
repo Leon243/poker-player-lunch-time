27139e7f23cfb76adf57958ecf485fd1767579fd
Baca, Pavol (pb478a) <pb478a@intl.att.com>
2019-10-25 10:46:42 +0200

    3

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,14 +1,32 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class Player {
 
-  static final String VERSION = "2";
+  static final String VERSION = "3";
 
   public static int betRequest(JsonElement request) {
-    if (Math.random() * 4 > 3) {
-      return 1;
+    JsonObject object = request.getAsJsonObject();
+    JsonArray players = object.get("players").getAsJsonArray();
+    boolean isPair = false;
+    for (JsonElement p : players) {
+      JsonElement holeCardsElement = p.getAsJsonObject().get("hole_cards");
+      if (holeCardsElement != null) {
+        JsonArray holeCards = holeCardsElement.getAsJsonArray();
+        isPair =
+            holeCards
+                .get(0)
+                .getAsJsonObject()
+                .get("rank")
+                .getAsString()
+                .equals(holeCards.get(0).getAsJsonObject().get("rank").getAsString());
+      }
+    }
+    if (isPair) {
+      return 200;
     }
     return 0;
   }
