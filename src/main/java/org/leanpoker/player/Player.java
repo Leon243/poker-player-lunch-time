1de9a865427850817d985ec3bc3557ce2d573c0f
Baca, Pavol (pb478a) <pb478a@intl.att.com>
2019-10-25 12:42:34 +0200

    aa

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,24 +1,20 @@
 package org.leanpoker.player;
 
-import com.google.gson.JsonArray;
+import java.util.Date;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Player {
 
-  static final String VERSION = "5";
+  static final String VERSION = new Date().toString();
 
   public static int betRequest(JsonElement request) {
     JsonObject object = request.getAsJsonObject();
-    JsonArray players = object.get("players").getAsJsonArray();
-    for (JsonElement p : players) {
-      JsonElement holeCardsElement = p.getAsJsonObject().get("hole_cards");
-      if (holeCardsElement != null) {
-        int stack = p.getAsJsonObject().get("stack").getAsInt();
-        return stack / 4;
-      }
-    }
-    return 0;
+    int inAction = object.get("in_action").getAsInt();
+    JsonObject player = object.get("players").getAsJsonArray().get(inAction).getAsJsonObject();
+    int stack = player.get("stack").getAsInt();
+    return (int) Math.floor(stack / 4);
   }
 
   public static void showdown(JsonElement game) {}
